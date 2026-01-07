0ddcfd0899468520bbb33f62d3f4d700bca8dac5
Abe <sd-hakonekoman@users.noreply.github.com>
2026-01-07 11:18:51 +0900

    index.html を src 配下に移動し、Vite の root を調整 #11 #6

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true, // ビルド前に出力ディレクトリを空にする
+  },
   resolve: {
     alias: {
       // path.resolve を使って絶対パスを指定
